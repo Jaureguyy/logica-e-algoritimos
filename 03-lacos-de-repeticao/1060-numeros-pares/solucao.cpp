7c9e7dd8b8e2ed12aca602248a39c138ea274312
João Pedro Jaureguy <jaureguy.jau@gmail.com>
2026-08-19 22:10:16 -0300

    Beecrowd 1060 - Numeros Pares

---
+++ 03-lacos-de-repeticao/1060-numeros-pares/solucao.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    double valor, positivos=0;
+
+    for(int i=0; i<6; i++){
+        cin >> valor;
+
+        if(valor>0){
+            positivos++;
+        }
+    }
+
+    cout << positivos << " valores positivos" << endl;
+}
