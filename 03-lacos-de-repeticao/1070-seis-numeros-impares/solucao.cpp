bebf8faa9817599f56240e456c3fb32058d3c7ca
João Pedro Jaureguy <jaureguy.jau@gmail.com>
2026-08-23 16:46:16 -0300

    Bee 1070 trocando while por for

--- 03-lacos-de-repeticao/1070-seis-numeros-impares/solucao.cpp
+++ 03-lacos-de-repeticao/1070-seis-numeros-impares/solucao.cpp
@@ -3,15 +3,13 @@ using namespace std;
 
 int main(){
 
-    int X, impar=0;
+    int X;
 
     cin >> X;
 
-    while(impar<6){
-        if(X%2!=0){
-            cout << X << endl;
-            impar++;
+    for(int i=X; i<=X+11; i++){
+        if(i%2!=0){
+            cout << i << endl;
         }
-        X++;
     }
 }
