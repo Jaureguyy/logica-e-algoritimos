4361b882345c72222d9864496e0678959c837a09
João Pedro Jaureguy <jaureguy.jau@gmail.com>
2026-08-23 14:40:00 -0300

    Beecrowd 1064 - Pares entre Cinco Numeros

---
+++ 03-lacos-de-repeticao/1065-pares-entre-cinco-numeros/solucao.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int valor, pares=0;
+
+    for(int i=0;i<5;i++){
+        cin >> valor;
+        if(valor%2==0){
+            pares++;
+        }
+    }
+
+    cout << pares << " valores pares" << endl;
+}
