da493c1b2c529832e0223dc65d09996dd52e2a58
João Pedro Jaureguy <jaureguy.jau@gmail.com>
2026-08-27 22:26:24 -0300

    1064 resolvido através de duas funções para media e positivos

--- 03-lacos-de-repeticao/1064-positivos-e-media/solucao.cpp
+++ 03-lacos-de-repeticao/1064-positivos-e-media/solucao.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+double media(double numerador, double denominador);
+bool ehPositivo(int valor);
 
 int main(){
 
-    double valor, positivos=0, numerador=0, denominador=0;
+    double soma=0, x;    
+    int positivos=0;
 
-    for(int i=0;i<6;i++){
-        cin >> valor;
-        
-        if(valor>0){
-            numerador+=valor;
-            denominador++;
+    for(int i=1; i<=6; i++){
+        cin >> x;
+
+        if(ehPositivo(x)){
             positivos++;
+            soma+=x;
         }
     }
 
+    cout << fixed << setprecision(1);
     cout << positivos << " valores positivos" << endl;
-    cout << numerador/denominador << endl;
+    cout << media(soma, positivos) << endl;
+
+}
+
+double media(double numerador, double denominador){
+    return numerador / denominador;
+}
+
+bool ehPositivo(int valor){
+    return valor>0;
 }
