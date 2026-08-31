bdb5aff909b4944790a6359bda8249291a4306ba
João Pedro Jaureguy <jaureguy.jau@gmail.com>
2026-08-31 19:27:15 -0300

    Beecrowd 1183 - Abve the main diagonal

---
+++ 05-matrizes-+-consolidacao/1183-above-the-main-diagonal/solucao.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void preencherMatriz(double m[][12]){
+    for(int i=0; i<12; i++){
+        for(int j=0; j<12; j++){
+            cin >> m[i][j];
+            }
+        }
+    }
+double somaMatriz(double m[][12]){
+    double soma=0;
+
+    for(int i=0; i<12; i++){
+        for(int j=0; j<12; j++){
+            if(j>i){
+                soma+=m[i][j];
+            }
+        }
+    }
+    return soma;
+}
+
+int main(){
+    double matriz[12][12];
+    char operacao;
+
+    cin >> operacao;
+    preencherMatriz(matriz);
+
+    cout << fixed << setprecision(1);
+    if(operacao=='S'){
+        cout << somaMatriz(matriz) << endl;
+    }
+    else if(operacao=='M'){
+        cout << somaMatriz(matriz)/66 << endl;
+    }
+}
